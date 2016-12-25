d7afcb24076f424c5d29be9af1237da726f959dc
Kalekulan <lukas.foughman@gmail.com>
2016-12-25 22:30:46 +0100

    Finally a working solution...

--- BlockCountries.sh
+++ BlockCountries.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#To delete rules, set action to delete
-#To add rules, set action to add
+#To delete rules, set action to 0
+#To add rules, set action to 1
 #The chosen path needs to be temporalily and should not include files that you don't want erased...
 #Inclusions can be added as a third argument as comma separated. They use the ISO standard. E.g. "se.zone,uk.zone"
 #Last argument is what port you want to use. Can be left empty
@@ -10,26 +10,26 @@ allowedCountries=$3
 port=$4
 echo $allowedCountries
 
-cd $path
-for countryCode in ${allowedCountries//,/ }
-do
-	wget http://www.ipdeny.com/ipblocks/data/countries/$countryCode
-done
-
-
+#cd $path
+for countryCode in ${allowedCountries//,/ }; do	wget http://www.ipdeny.com/ipblocks/data/countries/$countryCode -O $path$countryCode; done
 
 #while read line; do sudo ufw insert 1 deny from $line to any; done < cdir-china.txt
 
+cd $path
 for files in *.zone
 do
-	echo $files
+	#echo $files
 	#while read line; do echo $line; done < $Files
-	while read line
-	echo $line
-		if [ $action = "add" ]; do sudo ufw allow from $line to any port $port
-		else if [ $action = "delete" ];  do sudo ufw delete allow $line
+	while read line; do
+		#echo $line
+		if [ $action = "add" ]
+		then
+			sudo ufw allow from $line to any port $port
+		elif [ $action = "delete" ]
+		then
+			sudo ufw delete allow $line
 		fi
 	done < $files
 done
 
-rm $path/*.zone
+rm $path*.zone
