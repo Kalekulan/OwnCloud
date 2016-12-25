9bba3109370e953d0bec5262254f4d65f4b84a13
Kalekulan <lukas.foughman@gmail.com>
2016-12-25 21:13:31 +0100

    Remade the code
    Pick the countries you want to allow instead - resulting in blocking the
    rest

--- BlockCountries.sh
+++ BlockCountries.sh
@@ -2,28 +2,31 @@
 #To delete rules, set action to -1
 #To add rules, set action to 1
 #The chosen path needs to be temporalily and should not include files that you don't want erased...
-#Exclusions can be added as a third argument as comma separated. They use the ISO standard. E.g. "se.zone,uk.zone"
+#Inclusions can be added as a third argument as comma separated. They use the ISO standard. E.g. "se.zone,uk.zone"
+#Last argument is what port you want to use. Can be left empty
 action=$1
 path=$2
-exclusions="${3/,/ }"
-echo $exclusions
-wget http://www.ipdeny.com/ipblocks/data/countries/all-zones.tar.gz -O $path/all                                                                                                                                                                                                                                             -zones.tar.gz
-tar -zxvf $path/all-zones.tar.gz -C $path/
+allowedCountries=$3
+port=$4
+echo $allowedCountries
+for countryCode in $(echo $allowedCountries | sed "s/,/ /g")
+do
+	wget http://www.ipdeny.com/ipblocks/data/countries/$countryCode -O $path   
+done
+
 cd $path
-rm all-zones.tar.gz $exclusions
 
-#while read line; do sudo ufw insert 1 deny from $line to any; done < cdir-china                                                                                                                                                                                                                                             .txt
+#while read line; do sudo ufw insert 1 deny from $line to any; done < cdir-china.txt
 
-
-for files in .
+for files in *.zone
 do
         #echo $files
         #while read line; do echo $line; done < $Files
         while read line
-			if [$action -eq 1]; do sudo ufw insert 1 deny from $line to any
-			else if [$action -eq -1];  do sudo ufw delete deny $line
+			if [$action -eq 1]; do sudo ufw allow from $line to any port $port
+			else if [$action -eq -1];  do sudo ufw delete allow $line
 			fi
-		done < $files                                                                                                                                                                                                                                             les
+		done < $files
 done
 
-rm -r $path/*
+rm $path/*.zone
