90f44f5c2bd4861cae56e5b9a924469dc0de4776
Kalekulan <lukas.foughman@gmail.com>
2017-07-07 19:42:40 +0200

    Beautified + LF

--- BlockCountries.sh
+++ BlockCountries.sh
@@ -4,22 +4,17 @@
 #The chosen path needs to be temporalily and should not include files that you don't want erased...
 #Inclusions can be added as a third argument as comma separated. They use the ISO standard. E.g. "se.zone,uk.zone"
 #Last argument is what port you want to use. Can be left empty
-action=$1
-path=$2
-allowedCountries=$3
-port=$4
+action = $1
+path = $2
+allowedCountries = $3
+port = $4
+
 echo $allowedCountries
-
-#cd $path
 for countryCode in ${allowedCountries//,/ }; do	wget http://www.ipdeny.com/ipblocks/data/countries/$countryCode -O $path$countryCode; done
-
-#while read line; do sudo ufw insert 1 deny from $line to any; done < cdir-china.txt
-
 cd $path
 for files in *.zone
 do
-	#echo $files
-	#while read line; do echo $line; done < $Files
+
 	while read line; do
 		#echo $line
 		if [ $action = "add" ]
--- DataDriveHealthCheck.sh
+++ DataDriveHealthCheck.sh
@@ -32,9 +32,7 @@ if [ -z "$device" ]; then
 else
     echo Device: $device | tee -a $logFile
 fi
-#else
-#echo $String is NOT null.
-#fi     # $String is null.
+
 
 ls $device > /dev/null 2>&1
 lsExitCode = $?
@@ -58,7 +56,7 @@ if [[ $mntCode -eq 0 ]]; then
     echo Device: $device is mounted. Unmounting now... | tee -a $logFile
     sudo umount $device >> $logFile
     unmountCode = $?
-    
+
     if [[ $unmountCode -eq 0 ]]; then
         echo umount went OK | tee -a $logFile
     else
@@ -72,18 +70,13 @@ fi
 
 echo Starting fsck... | tee -a $logFile
 echo
-#fsCheck=$(sudo fsck.ext4 "$device")
 echo $timestamp > $printFile
 sudo fsck.ext4 -vn $device >> $printFile
-# > /dev/null 2>&1
 fsckCode = $?
 echo fsck exit code = $fsckCode | tee -a $logFile
 
-#echo $fsCheck
-
 if [[ $fsckCode -eq 0 ]]; then
     echo File system is all clean! | tee -a $logFile
-    #echo "$timestamp File system $device is all clean!" | tee -a $logFile
 else
     echo Fsck failed with exitcode $fsckCode. Exiting... | tee -a $logFile
     #sudo echo "$timestamp Fsck failed with exitcode $fsckCode. Exiting..." | tee -a $logFile
@@ -100,10 +93,10 @@ echo Done | tee -a $logFile
 exit
 
 SendMail() { #not used right now
-    
+
     error=$1
     path=$2
-    
+
     declare -a mailKeys=(
         mail_from_address
         mail_smtpmode
@@ -117,7 +110,7 @@ SendMail() { #not used right now
         mail_smtpsecure
     )
     declare -a mailValues
-    
+
     #arrayLength=${#mailKeys[@]}
     #i=0
     index=0
@@ -130,11 +123,11 @@ SendMail() { #not used right now
         echo ${mailValues[$index]}
         ((index++))
     done
-    
+
     #NAME=${MYVAR%*,}  # retain the part before the colon
     #NAME=${NAME##*>}  # retain the part after the last slash
     #NAME=${NAME//"'"}
     #echo $NAME
     return true
-    
+
 }
