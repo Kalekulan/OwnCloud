24cfb2ebc6d6ec24c2566acccc7e271238f2b0de
Kalekulan <lukas.foughman@gmail.com>
2017-07-07 18:29:45 +0200

    Beautified

--- SystemBackup.sh
+++ SystemBackup.sh
@@ -1,47 +1,29 @@
 #!/bin/bash
 
-#arg1 output path
+# arg1 output path
+# if database is password protected then create a file ~/.my.cnf with privilegies 600 and with the following content:
+# [mysqldump]
+# user=mysqluser
+# password=secret
 
-#if database is password protected then create a file ~/.my.cnf with privilegies 600 and with the following content:
-#[mysqldump]
-#user=mysqluser
-#password=secret
-
-
-#mountOutputPath="/var/systembackup/mountOutput.txt"
-rsyncOutputPath=$1
-#rsyncOutputPath="/mnt/STORAGE_ee7e0/nextcloud_backup"
-drive="STORAGE_ee7e0"
-date=$(date +\%Y\%m\%d)
-
-#> $mountOutputPath
-#mount | grep $drive > $mountOutputPath
-
-#echo Checking if $drive is mounted...
-#if [ ! -s $mountOutputPath ]
+rsyncOutputPath = $1
+drive = "STORAGE_ee7e0"
+date = $(date +\%Y\%m\%d)
 if ! mount | grep $drive; then
-	echo $drive is not mounted. I will give it a shot...
-	mount --all
-	#mount | grep $drive > $mountOutputPath
-	#if [ ! -s $mountOutputPath ]
-	if ! mount | grep $drive; then
-		echo Still cant mount. Check connection... Bye.
-		exit
-	fi
+    echo $drive is not mounted. I will give it a shot...
+    mount --all
+    if ! mount | grep $drive; then
+        echo Still cant mount. Check connection... Bye.
+        exit
+    fi
 fi
-#echo Kill server
-service apache2 stop
+
+service apache2 stop  #echo Kill server
 sleep 10
-#echo $drive is mounted... Executing rsync command.
-rsync -aAxXql --exclude-from=/var/rsync/rsyncExclusions.list /* $rsyncOutputPath/nextcloud_rsync_temp
-#echo Putting it in a tar...
-tar -cvpzf $rsyncOutputPath/nextcloud_backup_$date.tar.gz $rsyncOutputPath/nextcloud_rsync_temp
-#echo Syncing and backuping data directory...
-rsync -aAxXql /media/www/nextcloud/data/ $rsyncOutputPath/nextcloud_rsync_data
-#echo Taking a dump of mysql database.
-mysqldump -AR --events > $rsyncOutputPath/nextcloud_dbbackup_$date.sql
-#echo Let me zip that for you...
-gzip -f9 $rsyncOutputPath/nextcloud_dbbackup_$date.sql # > /mnt/STORAGE_ee7e0/nextcloud_backup/nextcloud_dbbackup_$(date +\%Y\%m\%d).sql.gz
-#echo Start server again.
-service apache2 start
+rsync -aAxXql --exclude-from=/var/rsync/rsyncExclusions.list /* $rsyncOutputPath/nextcloud_rsync_temp  #echo $drive is mounted... Executing rsync command.
+tar -cvpzf $rsyncOutputPath/nextcloud_backup_$date.tar.gz $rsyncOutputPath/nextcloud_rsync_temp  #echo Putting it in a tar...
+rsync -aAxXql /media/www/nextcloud/data/ $rsyncOutputPath/nextcloud_rsync_data  #echo Syncing and backuping data directory...
+mysqldump -AR --events > $rsyncOutputPath/nextcloud_dbbackup_$date.sql  #echo Taking a dump of mysql database.
+gzip -f9 $rsyncOutputPath/nextcloud_dbbackup_$date.sql  #echo Let me zip that for you...
+service apache2 start  #echo Start server again.
 #echo Script is done!
